206135e8b0f22970d474de8c5434d2662be108b8
Nikola Malic <nikola.malic95@gmail.com>
2021-04-24 00:52:59 +0200

    Remove virtual from contract method args

--- HackathonApp/Contracts/H4PToken.cs
+++ HackathonApp/Contracts/H4PToken.cs
@@ -10,63 +10,63 @@ namespace HackathonApp.Interfaces.Contract
         public class ApproveFunction : FunctionMessage
         {
             [Parameter("address", "spender", 1)]
-            public virtual string Spender { get; set; }
+            public string Spender { get; set; }
             [Parameter("uint256", "amount", 2)]
-            public virtual BigInteger Amount { get; set; }
+            public BigInteger Amount { get; set; }
         }
 
         [Function("decreaseAllowance", "bool")]
         public class DecreaseAllowanceFunction : FunctionMessage
         {
             [Parameter("address", "spender", 1)]
-            public virtual string Spender { get; set; }
+            public string Spender { get; set; }
             [Parameter("uint256", "subtractedValue", 2)]
-            public virtual BigInteger SubtractedValue { get; set; }
+            public BigInteger SubtractedValue { get; set; }
         }
 
         [Function("increaseAllowance", "bool")]
         public class IncreaseAllowanceFunction : FunctionMessage
         {
             [Parameter("address", "spender", 1)]
-            public virtual string Spender { get; set; }
+            public string Spender { get; set; }
             [Parameter("uint256", "addedValue", 2)]
-            public virtual BigInteger AddedValue { get; set; }
+            public BigInteger AddedValue { get; set; }
         }
 
         [Function("transfer", "bool")]
         public class TransferFunction : FunctionMessage
         {
             [Parameter("address", "recipient", 1)]
-            public virtual string Recipient { get; set; }
+            public string Recipient { get; set; }
             [Parameter("uint256", "amount", 2)]
-            public virtual BigInteger Amount { get; set; }
+            public BigInteger Amount { get; set; }
         }
 
         [Function("transferFrom", "bool")]
         public class TransferFromFunction : FunctionMessage
         {
             [Parameter("address", "sender", 1)]
-            public virtual string Sender { get; set; }
+            public string Sender { get; set; }
             [Parameter("address", "recipient", 2)]
-            public virtual string Recipient { get; set; }
+            public string Recipient { get; set; }
             [Parameter("uint256", "amount", 3)]
-            public virtual BigInteger Amount { get; set; }
+            public BigInteger Amount { get; set; }
         }
 
         [Function("allowance", "uint256")]
         public class AllowanceFunction : FunctionMessage
         {
             [Parameter("address", "owner", 1)]
-            public virtual string Owner { get; set; }
+            public string Owner { get; set; }
             [Parameter("address", "spender", 2)]
-            public virtual string Spender { get; set; }
+            public string Spender { get; set; }
         }
 
         [Function("balanceOf", "uint256")]
         public class BalanceOfFunction : FunctionMessage
         {
             [Parameter("address", "account", 1)]
-            public virtual string Account { get; set; }
+            public string Account { get; set; }
         }
 
         [Function("decimals", "uint8")]
@@ -85,64 +85,64 @@ namespace HackathonApp.Interfaces.Contract
         public class ApprovalEventDTO : IEventDTO
         {
             [Parameter("address", "owner", 1, true)]
-            public virtual string Owner { get; set; }
+            public string Owner { get; set; }
             [Parameter("address", "spender", 2, true)]
-            public virtual string Spender { get; set; }
+            public string Spender { get; set; }
             [Parameter("uint256", "value", 3, false)]
-            public virtual BigInteger Value { get; set; }
+            public BigInteger Value { get; set; }
         }
 
         [Event("Transfer")]
         public class TransferEventDTO : IEventDTO
         {
             [Parameter("address", "from", 1, true)]
-            public virtual string From { get; set; }
+            public string From { get; set; }
             [Parameter("address", "to", 2, true)]
-            public virtual string To { get; set; }
+            public string To { get; set; }
             [Parameter("uint256", "value", 3, false)]
-            public virtual BigInteger Value { get; set; }
+            public BigInteger Value { get; set; }
         }
 
         [FunctionOutput]
         public class AllowanceOutputDTO : IFunctionOutputDTO
         {
             [Parameter("uint256", "", 1)]
-            public virtual BigInteger ReturnValue1 { get; set; }
+            public BigInteger ReturnValue1 { get; set; }
         }
 
         [FunctionOutput]
         public class BalanceOfOutputDTO : IFunctionOutputDTO
         {
             [Parameter("uint256", "", 1)]
-            public virtual BigInteger ReturnValue1 { get; set; }
+            public BigInteger ReturnValue1 { get; set; }
         }
 
         [FunctionOutput]
         public class DecimalsOutputDTO : IFunctionOutputDTO
         {
             [Parameter("uint8", "", 1)]
-            public virtual byte ReturnValue1 { get; set; }
+            public byte ReturnValue1 { get; set; }
         }
 
         [FunctionOutput]
         public class NameOutputDTO : IFunctionOutputDTO
         {
             [Parameter("string", "", 1)]
-            public virtual string ReturnValue1 { get; set; }
+            public string ReturnValue1 { get; set; }
         }
 
         [FunctionOutput]
         public class SymbolOutputDTO : IFunctionOutputDTO
         {
             [Parameter("string", "", 1)]
-            public virtual string ReturnValue1 { get; set; }
+            public string ReturnValue1 { get; set; }
         }
 
         [FunctionOutput]
         public class TotalSupplyOutputDTO : IFunctionOutputDTO
         {
             [Parameter("uint256", "", 1)]
-            public virtual BigInteger ReturnValue1 { get; set; }
+            public BigInteger ReturnValue1 { get; set; }
         }
     }
 }
